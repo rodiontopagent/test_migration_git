373b9fc20b114d13ebfcfb99d4ba1f004d9b813a
rodiontopagent <rodion@top-agt.com>
2016-09-23 12:02:13 +0300

    add airport table

--- app/database/migrations/2016_09_23_085120_airport.php
+++ app/database/migrations/2016_09_23_085120_airport.php
@@ -5,24 +5,35 @@ use Illuminate\Database\Migrations\Migration;
 
 class Airport extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		//
-	}
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('airport', function ($table) {
+            $table->increments('id');
+            $table->string('name', 255);
+            $table->string('name_heb', 255);
+            $table->integer('city_id');
+            $table->integer('country_id');
+            $table->string('code', 10);
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at');
+            $table->timestamp('deleted_at')->nullable()->default(DB::raw('NULL'));
+            $table->index(['code', 'city_id', 'country_id']);
+        });
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		//
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('airport');
+    }
 
 }
