aeb9fa5eaa82dac6810df067b6099750104a2820
rodiontopagent <rodion@top-agt.com>
2016-09-23 12:25:25 +0300

    add bank table

---
+++ app/database/migrations/2016_09_23_092253_bank.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Bank extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bank', function ($table) {
+            $table->increments('id');
+            $table->string('name', 255);
+            $table->string('code', 255);
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at');
+            $table->timestamp('deleted_at')->nullable()->default(DB::raw('NULL'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('bank');
+    }
+
+}
