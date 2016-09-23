a35c46b673f892c78a9a5f3b99e82df229fda000
rodiontopagent <rodion@top-agt.com>
2016-09-23 12:30:16 +0300

    add cache table

---
+++ app/database/migrations/2016_09_23_092725_cache.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Cache extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cache', function ($table) {
+            $table->string('key', 255);
+            $table->text('value');
+            $table->integer('expiration');
+            $table->index('key');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('cache');
+    }
+
+}
