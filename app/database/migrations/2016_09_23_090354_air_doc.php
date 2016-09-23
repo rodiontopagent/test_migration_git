16938a46208267eadd7d8ba36bba4d8f10ca72a0
rodiontopagent <rodion@top-agt.com>
2016-09-23 12:06:48 +0300

    add air_doc table

---
+++ app/database/migrations/2016_09_23_090354_air_doc.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AirDoc extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('air_doc', function ($table) {
+            $table->increments('id');
+            $table->text('data');
+            $table->string('entity', 10);
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at');
+            $table->timestamp('deleted_at')->nullable()->default(DB::raw('NULL'));
+            $table->index('entity');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('air_doc');
+    }
+
+}
