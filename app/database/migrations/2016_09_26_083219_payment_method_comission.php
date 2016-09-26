dd65a5bb78693ba26cd445c82284223c9b88deb1
rodiontopagent <rodion@top-agt.com>
2016-09-26 11:45:24 +0300

    add last tables

---
+++ app/database/migrations/2016_09_26_083219_payment_method_comission.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class PaymentMethodComission extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('payment_method_comission', function($table) {
+            $table->increments('id');
+
+            $table->integer('payment_method_id');
+            $table->index('payment_method_id');
+
+            $table->decimal('rate', 10, 2);
+            $table->integer('min_payments');
+            $table->integer('max_payments');
+
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->nullable()->default(DB::raw('NULL'));
+            $table->timestamp('deleted_at')->nullable()->default(DB::raw('NULL'));
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('payment_method_comission');
+	}
+
+}
